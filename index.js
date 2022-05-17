8db0215402efa428f0d0fdd8edf8a12af4b80c0e
Omarfaruk4200 <emon35293@gmail.com>
2022-05-17 18:00:11 +0600

    added inventory

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function run() {
             res.send(result)
         })
 
-        // deleted it
+        // deleted ite
         app.delete("/inventory/:id", async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }

88e2176efa3eac5c87e4e9e20cb09258254ffd81
Omarfaruk4200 <emon35293@gmail.com>
2022-05-17 17:59:32 +0600

    added find

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function run() {
             res.send(result)
         })
 
-        // deleted
+        // deleted it
         app.delete("/inventory/:id", async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }

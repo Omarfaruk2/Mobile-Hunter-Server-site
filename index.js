60ac21814532bfeb4cbed02e0ae7776c9498cc0c
Omarfaruk4200 <emon35293@gmail.com>
2022-05-17 18:06:21 +0600

    added id api

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
             res.send(result)
         })
 
-        // details
+        // details item
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
